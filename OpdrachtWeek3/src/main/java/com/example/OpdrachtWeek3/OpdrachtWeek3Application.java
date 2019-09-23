665440bbbffa759a2c2f2efaab5d5844928db0c8
Scizivire <danilolima@live.nl>
2019-09-23 16:59:30 +0200

    Skelet van project

---
+++ OpdrachtWeek3/src/main/java/com/example/OpdrachtWeek3/OpdrachtWeek3Application.java
@@ -0,0 +1,13 @@
+package com.example.OpdrachtWeek3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpdrachtWeek3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpdrachtWeek3Application.class, args);
+	}
+
+}
